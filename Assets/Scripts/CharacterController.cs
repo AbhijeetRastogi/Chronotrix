caf5fecf6dc2abad19d74f9d6b086db9ab4b489c
AbhijeetRastogi <abhirastogi800@gmail.com>
2023-03-15 19:15:40 +0530

    Simplifying move and jump
    using Translation, removing other BT give code

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -10,96 +10,69 @@ using UnityEngine.TextCore;
 
 public class CharacterController : MonoBehaviour
 {
-    public static event Action<int> OnGround;
     
-    [SerializeField] private GameObject top;
-    [SerializeField] private GameObject bottom;
     [SerializeField] private int jumpHeight = 3;
     [SerializeField] private float moveTime = 0.3f;
     
     private int horizontal;
-    private float atRest;
     private float timeL;
     private float timeR;
-    private float time;
     private bool isJump;
     private bool onGround = true;
-    private bool isRoof;
-    private int currentHeight;
+    
 
-
-    // private void OnEnable()
-    // {
-    //     Collision.OnGround += OnGround(false);
-    // }
-    // private void OnDisable()
-    // {
-    //     Collision.OnGround -= OnGround(true);
-    // }
-
-    private void Start()
+    private void Start()    
     {
         timeL = 0.0f;
         timeR = 0.0f;
-        time = 0.0f;
+        
     }
-    
-    
+
+
+    private void Update()
+    {
+        horizontal = (int)Input.GetAxisRaw("Horizontal");
+        isJump = (Input.GetButtonDown("Jump")) ? true : false;
+    }
+
     private void FixedUpdate()
     {
-        horizontal = (int) Input.GetAxisRaw("Horizontal");
-        isJump = (Input.GetButtonDown("Jump")) ? true : false;
-        
         // horizontal 
         timeL += Time.deltaTime;
         timeR += Time.deltaTime;
-        time += Time.deltaTime;
-
+        
         if (timeL > moveTime && horizontal == -1)
         {
-            transform.position += new Vector3(horizontal, 0, 0);
+            transform.Translate(Vector3.left);
             timeL = 0;
         }
         if(timeR > moveTime && horizontal == 1)
         {
-            transform.position += new Vector3(horizontal, 0, 0);
+            transform.Translate(Vector3.right);
             timeR = 0;
         }
 
-        
         // jump
-        if (onGround && isJump && !isRoof && currentHeight < jumpHeight)
+        if (onGround && isJump)
         {
-            currentHeight++;
-            transform.position += new Vector3(0, 1, 0);
-            
+            transform.Translate(Vector3.up * jumpHeight);
         }
-        // gravity
-        if (!onGround && time > moveTime )
-        {
-            transform.position += Vector3.down * 1;
-            
-            time = 0;
-        }
-         
     }
 
-    public void OnChildTriggerEnter(int whichCollider, Vector3 position)
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        if(whichCollider==0)
+        if (col.gameObject.tag == "Ground")
+        {
             onGround = true;
-        else
-            isRoof = true;
+        }
     }
 
-    public void OnChildTriggerExit(int whichCollider, Vector3 position)
+    private void OnCollisionExit2D(Collision2D col)
     {
-        if(whichCollider==0)
+        if (col.gameObject.tag == "Ground")
+        {
             onGround = false;
-        else
-            isRoof = false;
+        }
     }
-
-
 }
 
