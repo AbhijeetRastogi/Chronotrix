6ca69f6a7ecf98defbba60a55c00d4ace4747aa4
AbhijeetRastogi <abhirastogi800@gmail.com>
2023-03-17 17:02:32 +0530

    moving with Velocity

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -13,66 +13,116 @@ public class CharacterController : MonoBehaviour
     
     [SerializeField] private int jumpHeight = 3;
     [SerializeField] private float moveTime = 0.3f;
-    
+
+    private GameObject gm;
+    private Rigidbody2D rgbody;
+    private float movementSpeed;
     private int horizontal;
     private float timeL;
     private float timeR;
     private bool isJump;
-    private bool onGround = true;
+    private bool onGround;
+    private int time = 0;
     
 
+    
+    public int GetTime()
+    {
+        return time;
+    }
+    
     private void Start()    
     {
+        gm = gameObject;
+        rgbody = GetComponent <Rigidbody2D>();
         timeL = 0.0f;
         timeR = 0.0f;
-        
+        movementSpeed = 10f;
+        time = 0;
     }
-
+    
 
     private void Update()
     {
         horizontal = (int)Input.GetAxisRaw("Horizontal");
-        isJump = (Input.GetButtonDown("Jump")) ? true : false;
+        isJump = (Input.GetButtonDown("Jump") && onGround) ? true : isJump;
     }
 
     private void FixedUpdate()
     {
+        CheckHorizontalMovement();
+        CheckJumpMovement();
+    }
+    
+    private void CheckHorizontalMovement()
+    {
+
         // horizontal 
         timeL += Time.deltaTime;
         timeR += Time.deltaTime;
         
         if (timeL > moveTime && horizontal == -1)
         {
-            transform.Translate(Vector3.left);
+            Move(Vector3.left * movementSpeed);
+            time--;
             timeL = 0;
         }
         if(timeR > moveTime && horizontal == 1)
         {
-            transform.Translate(Vector3.right);
+            Move(Vector3.right * movementSpeed);
+            time++;
             timeR = 0;
         }
 
+        
+    }
+
+    private void CheckJumpMovement()
+    {
         // jump
-        if (onGround && isJump)
+        if (isJump)
         {
-            transform.Translate(Vector3.up * jumpHeight);
+            Move(Vector3.up * jumpHeight);
+            isJump = false;
+        }
+        
+        //snappy gravity
+        if (!onGround)
+        {
+            Move(Vector3.down );
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D col)
+    private void Move(Vector3 direction)
     {
-        if (col.gameObject.tag == "Ground")
+        StartCoroutine(MoveCoroutine(direction));
+    }
+    
+    IEnumerator MoveCoroutine(Vector3 direction)
+    {
+        print("moving");
+        Vector3 currentPosition = gm.transform.position;
+        rgbody.velocity = direction;
+        while (true)
         {
-            onGround = true;
+            Vector3 newPosition = gm.transform.position;
+            if (Vector3.Distance(currentPosition, newPosition) > Vector3.Magnitude(direction))
+            {
+                rgbody.velocity = Vector3.zero;
+            }
+            yield return new WaitForSeconds(0.02f);
         }
     }
 
-    private void OnCollisionExit2D(Collision2D col)
+    
+    private void OnTriggerEnter2D(Collision2D col)
     {
-        if (col.gameObject.tag == "Ground")
-        {
-            onGround = false;
-        }
+        onGround = true;
+    }
+
+    private void OnTriggerExit2D(Collision2D col)
+    {
+        onGround = false;
     }
 }
 
